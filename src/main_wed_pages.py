8a5b4c41955c8f3e917100d05a7cfd1cacebe522
Ilya Gaifullin <proskmax@yandex.ru>
2024-10-03 21:18:37 +0500

    -#

--- src/main_wed_pages.py
+++ src/main_wed_pages.py
@@ -1,4 +1,4 @@
-from web_pages.web_pages import card_number
-
-if __name__ == "__main__":
-    print(hello_user())
+# from web_pages.web_pages import card_number
+#
+# if __name__ == "__main__":
+#     print(hello_user())
