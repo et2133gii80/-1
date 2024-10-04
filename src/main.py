b8f9cb749bb6cb7676ba1df82b291f310c46005a
prosk <prokmax@yandex.ru>
2024-10-04 12:38:28 +0300

    test_github

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ rel_excel_file_path = os.path.join(current_excel, "../data/operations.xlsx")
 excel_file_path = os.path.abspath(rel_excel_file_path)
 
 
-# def main():
-#     df= read_xlsx(excel_file_path)
-#     print(card_number(df))
-#
-# if __name__ =="__main__":
-#     main()
+def main():
+    df= read_xlsx(excel_file_path)
+    print(card_number(df))
+
+if __name__ =="__main__":
+    main()
